9ac9b030de2b9e3255e95e923d0a42b163c7444e
Kevin Siswandi <4892798+Physicist91@users.noreply.github.com>
2024-04-07 06:18:42 +0000

    fix import

--- linkedin.py
+++ linkedin.py
@@ -13,7 +13,7 @@ from selenium.webdriver.common.by import By
 
 from config import settings
 from basecrawler import BaseAbstractCrawler
-from documents import PostDocument
+from documents import PostDocument, ProfileDocument
 
 # set up the Google Cloud Logging python client library
 import google.cloud.logging
